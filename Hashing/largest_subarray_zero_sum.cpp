6b0aa2963d6cafbb38accdc1ab6d312fa10da94b
kuluruvineeth <kuluruvineeth8623@gmail.com>
2021-08-14 08:28:25 +0530

    largest subarray with zero sum

---
+++ Hashing/largest_subarray_zero_sum.cpp
@@ -0,0 +1,38 @@
+/* PROBLEM STATEMENT : Given an array of numbers find the largest subarray with 0 sum*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//optimal approach
+int maxlen(vector<int> &v, int n)
+{
+  int sum = 0, maxi = 0;
+  unordered_map<int, int> m;
+  for (int i = 0; i < n; i++)
+  {
+    sum += v[i];
+    if (sum == 0)
+    {
+      maxi = i + 1;
+    }
+    else
+    {
+      if (m.find(sum) != m.end())
+      {
+        maxi = max(maxi, i - m[sum]);
+      }
+      else
+      {
+        m[sum] = i;
+      }
+    }
+  }
+  return maxi;
+}
+
+int main()
+{
+  vector<int> v{15, -2, 2, -8, 1, 7, 10, 23};
+  cout << maxlen(v, v.size()) << endl;
+  return 0;
+}

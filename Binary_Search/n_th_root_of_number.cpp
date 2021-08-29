3b7abf99a0cdc2592eadd636b0fbb102337932a6
kuluruvineeth <kuluruvineeth8623@gmail.com>
2021-08-29 08:01:06 +0530

    nth root of number using binary search

---
+++ Binary_Search/n_th_root_of_number.cpp
@@ -0,0 +1,47 @@
+/* PROBLEM STATEMENT : Given a number find nth root of that number */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+double multiply(double number, int n)
+{
+  double ans = 1.0;
+  for (int i = 1; i <= n; i++)
+  {
+    ans = ans * number;
+  }
+  return ans;
+}
+
+double getNthRoot(int n, int m)
+{
+  double low = 1;
+  double high = m;
+  double diff = 1e-6;
+  double mid;
+  while ((high - low) > diff)
+  {
+    mid = (low + high) / 2.0;
+    if (multiply(mid, n) < m)
+    {
+      low = mid;
+    }
+    else
+    {
+      high = mid;
+    }
+  }
+  cout << low << " " << high << endl;
+
+  //checking
+  cout << pow(m, (double)(1.0 / (double)n)) << endl;
+  return mid;
+}
+
+int main()
+{
+  int n, m;
+  cin >> n >> m;
+  cout << getNthRoot(n, m);
+  return 0;
+}

d5a99027a6a09a11f5dd311d3f3f44d393f9eaa6
kuluruvineeth <kuluruvineeth8623@gmail.com>
2021-09-03 11:44:50 +0530

    level order traversal

---
+++ Trees/level_order_traversal.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+  vector<vector<int>> levelorder(TreeNode *root)
+  {
+    vector<vector<int>> ans;
+    if (root == NULL)
+      return ans;
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+      int size = q.size();
+      vector<int> level;
+      for (int i = 0; i < size; i++)
+      {
+        TreeNode *node = q.front();
+        q.pop();
+        if (node->left != NULL)
+          q.push(node->left);
+        if (node->right != NULL)
+          q.push(node->right);
+        level.push_back(node->val);
+      }
+      ans.push_back(level);
+    }
+    return ans;
+  }
+};
+
+int main()
+{
+  TreeNode *root = new TreeNode(1);
+  root->left = new TreeNode(2);
+  root->right = new TreeNode(0);
+  root->left->left = new TreeNode(7);
+  root->left->right = new TreeNode(9);
+
+  Solution s;
+  vector<vector<int>> ans = s.levelorder(root);
+  for (int i = 0; i < ans.size(); i++)
+  {
+    for (int j = 0; j < ans[i].size(); j++)
+    {
+      cout << ans[i][j] << " ";
+    }
+    cout << endl;
+  }
+  cout << endl;
+  return 0;
+}

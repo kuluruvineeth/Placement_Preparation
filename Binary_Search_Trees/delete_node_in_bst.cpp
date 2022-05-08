9707a802d235879a19c9db426b301b1a926feafc
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-05-08 06:23:41 +0530

    delete node in bst

--- Binary_Search_Trees/delete_node_in_bst.cpp
+++ Binary_Search_Trees/delete_node_in_bst.cpp
@@ -1,74 +1,3 @@
-# List of Binary Search Trees questions for Interview Preparation
-
-## 1. [Search and Insert](https://github.com/kuluruvineeth/Placement_Preparation/blob/main/Binary_Search_Trees/search_and_insert.cpp)
-```cpp
-#include<bits/stdc++.h>
-using namespace std;
-
-typedef struct node
-{
-    int data;
-    struct node *left;
-    struct node *right;
-    node(int d)
-    {
-        data = d;
-        left = right = NULL;
-    }
-}no;
-
-void inorderTraversal(no *root)
-{
-    if(root==NULL) return;
-    inorderTraversal(root->left);
-    cout<<root->data<<" ";
-    inorderTraversal(root->right);
-}
-/*
-    using iteration
-    TC: O(log N) not skewed
-        O(N) for skewed
-*/
-void insert(no *root,int key)
-{
-    no *newnode = new node(key);
-    if(root==NULL)
-    {
-        root=newnode;
-        return;
-    }
-    no *prev=NULL;
-    no *temp=root;
-    while(temp!=NULL)
-    {
-        if(temp->data > key)
-        {
-            prev=temp;
-            temp=temp->left;
-        }
-        else if(temp->data < key)
-        {
-            prev=temp;
-            temp=temp->right;
-        }
-    }
-    if(prev->data > key) prev->left = newnode;
-    else prev->right = newnode;
-}
-
-int main()
-{
-    no *root = new node(5);
-    insert(root,10);
-    insert(root,12);
-    insert(root,2);
-    inorderTraversal(root);
-    return 0;
-}
-```
-
-## 2. [Delete node in BST](https://github.com/kuluruvineeth/Placement_Preparation/blob/main/Binary_Search_Trees/delete_node_in_bst.cpp)
-```cpp
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -171,4 +100,3 @@ int main()
     inorderTraversal(root1);
     return 0;
 }
-```

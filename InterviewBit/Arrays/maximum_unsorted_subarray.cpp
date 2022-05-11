45c0e394b08b717683525adb93cb9f2ee8cffc23
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-05-11 07:34:29 +0530

    maximum unsorted subarray

---
+++ InterviewBit/Arrays/maximum_unsorted_subarray.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> subUnsort(vector<int> &A)
+{
+    vector<int> ans;
+    int n=A.size();
+    int i=0,j=n-1;
+    while(i<n-1 && A[i]<=A[i+1]) i++;
+    while(j>0 and A[j]>=A[j-1]) j--;
+    if(i==n-1)
+    {
+        ans.push_back(-1);
+        return ans;
+    }
+    int mn=A[i+1],mx=A[i+1];
+    for(int k=i;k<=j;k++)
+    {
+        mx = max(mx,A[k]);
+        mn = min(mn,A[k]);
+    }
+    int l=0,r=n-1;
+    while(A[l]<=mn and l<=i) l++;
+    while(A[r]>=mx and r>=j) r--;
+    ans.push_back(l);
+    ans.push_back(r);
+    return ans;
+}
+
+int main()
+{
+    vector<int> v = {1,3,2,4,5};
+    vector<int> ans = subUnsort(v);
+    for(int i=0;i<ans.size();i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    return 0;
+}

e6be5a43cb0e96ed2cd497a673d292a98554e154
Kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-10-13 08:16:20 +0530

    sum of round numbers

---
+++ codeforces/Implementation(Constructive)/sumOfRoundNumbers.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> v;
+        int count=0,remain=0;
+        while(n!=0)
+        {
+            remain = n%10;
+            if(remain*pow(10,count) > 0){
+                v.emplace_back(remain*pow(10,count));
+            }
+            count++;
+            n=n/10;
+        }
+        cout<<v.size()<<endl;
+        for(auto num:v){
+            cout<<num<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
